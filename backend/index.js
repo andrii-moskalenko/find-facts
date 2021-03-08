c744d92a01d20b330b039c19d2d077218ffb1a68
andrii-moskalenko <anm@DigiDom.dk>
2021-03-09 00:38:51 +0200

    add server

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+const cors = require('cors')({ origin: true});
+
+const cheerio = require('cheerio');
+const getUrls = require('get-urls');
+const fetch = require('node-fetch');
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const scrapeMetatags = (text) => {
+
+
+      const urls = ['https://www.politico.com/politics'];
+
+      const requests = urls.map(async url => {
+          
+          const res = await fetch(url, {
+            method: 'get',
+            headers: { 
+                'Content-Type': 'text/plain; charset=windows-1252',
+
+            },
+        });
+
+          const html = await res.text();
+          const $ = cheerio.load(html);
+          
+          const getMetatag = (name) =>  
+              $(`meta[name=${name}]`).attr('content') ||  
+              $(`meta[property="og:${name}"]`).attr('content') ||  
+              $(`meta[property="twitter:${name}"]`).attr('content');
+
+          return { 
+              url,
+              title: $('title').first().text(),
+              favicon: $('link[rel="shortcut icon"]').attr('href'),
+              description: getMetatag('description'),
+              image: getMetatag('image'),
+              author: getMetatag('author'),
+          }
+      });
+  
+  
+      return Promise.all(requests);
+  
+  
+}
+
+app.get('/', (req, res) => {
+    scrapeMetatags()
+    .then(data => res.send(data))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
