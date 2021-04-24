7a3ca0041f3e9e0c2958c8899c70692b9667bb0b
andrii-moskalenko <anm@DigiDom.dk>
2021-04-24 15:51:42 +0300

    setup environments

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,20 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { INews } from './app.consts';
+import { environment } from 'src/environments/environment';
 @Injectable({providedIn: 'root'})
 export class AppService {
     constructor(private http: HttpClient) {
     }
 
     public getNews(): Observable<INews[]> {
-        return this.http.get<INews[]>('/proxy/news');
+        return this.http.get<INews[]>(`${environment.baseUrl}/news`);
     }
 
     public getArticle(title): Observable<INews> {
-        return this.http.get<INews>(`/proxy/news/article?title=${title}`);
+        return this.http.get<INews>(`${environment.baseUrl}/news/article?title=${title}`);
     }
 
     public getEvents() {
-        return this.http.get<INews[]>('/proxy/events');
+        return this.http.get<INews[]>(`${environment.baseUrl}/events`);
     }
 }
